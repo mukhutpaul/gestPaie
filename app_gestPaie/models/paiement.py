c585df72198d34edbb9624f4e5d378fddacc2a5d
edosonde <christianedosonde07@gmail.com>
2024-02-21 01:27:03 +0100

    slt

--- app_gestPaie/models/paiement.py
+++ app_gestPaie/models/paiement.py
@@ -2,6 +2,6 @@ from django.db import models
 from app_gestPaie.models.frais import *
 
 class paiment(models.Model):
-    codeFrais = models.ForeignKey(Frais,on_delete=models.CASCADE)
+    codeFrais = models.Foreignkey(Frais,on_delete=models.CASCADE)
     datepaie = models.CharField(max_length=20)
     montant = models.CharField(max_length=10)
