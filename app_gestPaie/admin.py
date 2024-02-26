e25c8be72f7e35a32f59e1ebf54cd1ccee9dde41
mukhut <mukhutjp@gmail.com>
2024-02-26 18:42:46 +0400

    modif

--- app_gestPaie/admin.py
+++ app_gestPaie/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app_gestPaie.models.paiement import *
+from app_gestPaie.models.classe import *
+from app_gestPaie.models.frais import *
+from app_gestPaie.models.eleve import *
 
 # Register your models here.
+
+admin.site.register(paiment)
+admin.site.register(Classe)
+admin.site.register(Frais)
+admin.site.register(Eleve)
--- gestPaie/urls.py
+++ gestPaie/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_gestPaie.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index,name="index"),
 ]
